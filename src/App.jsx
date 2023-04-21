7da71f9a0afd96f4a50aa40a24882178099e8a5c
Vikas Belal <belal.vikas.5@gmail.com>
2023-04-21 08:36:40 +0530

    Completed app basic setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+// font-family: 'Open Sans', sans-serif;
+function App() {
+
+  return (
+    <>
+      Basic setup for Blog application is ready
+    </>
+  )
+}
+
+export default App
