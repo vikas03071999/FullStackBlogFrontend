fdd193dd2efc996374d0986e5df6f4cb27dcc7e2
Vikas Belal <belal.vikas.5@gmail.com>
2023-04-27 07:22:02 +0530

    Implemented Login and Signup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
 import './App.css'
+import Blogs from './Pages/Blogs'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // font-family: 'Open Sans', sans-serif;
 function App() {
 
   return (
-    <>
-      Basic setup for Blog application is ready
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path = "/" element = {<Register />} />
+        <Route path = "/Signup-blog-app" element = {<Register />} />
+        <Route path = "/Login-blog-app" element = {<Login />} />
+        <Route path = "/Blogs-home" element={<Blogs />} />
+      </Routes>  
+    </BrowserRouter>
   )
 }
 
