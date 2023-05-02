d76fd40c8c91a477c6382f7632573030bfad59e1
Vikas Belal <belal.vikas.5@gmail.com>
2023-05-02 08:47:51 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import './App.css'
 import Blogs from './Pages/Blogs'
 import Login from './Pages/Login'
 import Register from './Pages/Register'
+import WriteBlog from './Pages/WriteBlog'
+import BlogDetails from './Pages/BlogDetails'
+import UserProfile from './Pages/UserProfile'
 import {BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // font-family: 'Open Sans', sans-serif;
@@ -13,7 +15,10 @@ function App() {
         <Route exact path = "/" element = {<Register />} />
         <Route path = "/Signup-blog-app" element = {<Register />} />
         <Route path = "/Login-blog-app" element = {<Login />} />
-        <Route path = "/Blogs-home" element={<Blogs />} />
+        <Route path = "/Blogs-home" element = {<Blogs />} />
+        <Route path = "/Write-blog" element = {<WriteBlog />}/>
+        <Route path = "/Blog-detail" element = {<BlogDetails />} />
+        <Route path = "/My-profile" element = {<UserProfile />} />
       </Routes>  
     </BrowserRouter>
   )
